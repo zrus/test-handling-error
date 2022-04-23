a52247a6d12a3bcb3ceb952dcff69b509f935c35
Tuong Nguyen <tuongn09@gmail.com>
2022-04-23 18:52:16 +0700

    add error anylizing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Error;
 use derive_more::{Display, Error};
 use gst::prelude::{Cast, ElementExt, GObjectExtManualGst, GstBinExt, GstObjectExt};
 
+use std::env;
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
 struct ErrorMessage {
@@ -11,26 +12,71 @@ struct ErrorMessage {
     source: gst::glib::Error,
 }
 
-fn main() {
-    use std::env;
+#[derive(Debug)]
+enum GStreamerError {
+    Retriable(String),
+    Irretriable(String),
+}
 
+impl From<gst::glib::Error> for GStreamerError {
+    fn from(err: gst::glib::Error) -> Self {
+        unsafe {
+            let raw = err.into_raw();
+            match (*raw).domain {
+                2434 => Self::Irretriable(String::from("URI contains invalid protocol")),
+                2626 => match (*raw).code {
+                    5 => Self::Irretriable(String::from("URI contains invalid path")),
+                    7 => Self::Irretriable(String::from("URI contains invalid domain")),
+                    _ => Self::Irretriable(String::from("URI invalid")),
+                },
+                2628 => Self::Irretriable(String::from("Unauthorized")),
+                2593 => Self::Irretriable(String::from("URI contains invalid port")),
+                _ => Self::Irretriable(String::from("Unhandled error")),
+            }
+        }
+    }
+}
+
+fn main() {
     let mut args = env::args();
     let _arg0 = args.next().unwrap();
 
     let uri = args.next().expect("No input URI provided");
     let username = args.next();
     let password = args.next();
+    let num_thread = args.next();
+    let num_thread = num_thread.unwrap().parse::<u8>().unwrap();
 
-    match create_pipeline(&uri, username.as_deref(), password.as_deref()).and_then(main_loop) {
-        Ok(r) => r,
-        Err(e) => eprintln!("Error: {}", e),
+    let mut handlers = Vec::new();
+
+    for i in 0..num_thread {
+        let uri = uri.clone();
+        let username = username.clone();
+        let password = password.clone();
+        handlers.push(std::thread::spawn(move || {
+            match create_pipeline(&uri, username.as_deref(), password.as_deref(), i)
+                .and_then(main_loop)
+            {
+                Ok(r) => r,
+                // Err(e) => eprintln!("Error: {}", e),
+                Err(_) => {}
+            }
+        }))
+    }
+
+    for handler in handlers {
+        handler.join().unwrap();
     }
 }
+
+/// Wrong domain, wrong path, wrong port, wrong protocol, wrong authen info are need to stop absolutely.
+/// Most of others else needs to be retried.
 
 fn create_pipeline(
     uri: &str,
     username: Option<&str>,
     password: Option<&str>,
+    seq: u8,
 ) -> Result<gst::Pipeline, Error> {
     gst::init()?;
 
@@ -56,8 +102,14 @@ fn create_pipeline(
     sink.set_callbacks(
         gst_app::AppSinkCallbacks::builder()
             .new_sample(move |appsink| match appsink.pull_sample() {
-                Ok(_) => Ok(gst::FlowSuccess::Ok),
-                Err(_) => Err(gst::FlowError::Error),
+                Ok(_) => {
+                    println!("thread {}: ok", seq);
+                    Ok(gst::FlowSuccess::Ok)
+                }
+                Err(_) => {
+                    eprintln!("thread {}: error", seq);
+                    Err(gst::FlowError::Error)
+                }
             })
             .build(),
     );
@@ -80,6 +132,7 @@ fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
             }
             MessageView::Error(err) => {
                 pipeline.set_state(gst::State::Null)?;
+                eprintln!("{:?}", GStreamerError::from(err.error()));
                 return Err(ErrorMessage {
                     src: msg
                         .src()
